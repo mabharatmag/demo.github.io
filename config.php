89de930d542ec9294c926d391c7b67e4eb3c74fb
mabharatmag <mahabharatkgp@gmail.com>
2021-05-18 22:14:54 +0530

    file
    f

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "mysql";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if (!$conn) { // If Check Connection
+    die("<script>alert('Connection Failed.')</script>");
+}
+
+?>
+
+
+
+
